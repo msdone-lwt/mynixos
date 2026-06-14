60e576a1fa4f052664a9709b476ead09c99d87c1
msdone <lwt6077@gmail.com>
2026-06-15 02:02:24 +0800

    hm: fix tmux plugin
    Signed-off-by: msdone <lwt6077@gmail.com>

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -1,4 +1,4 @@
-{ 
+{
   pkgs,
   config,
   ...
@@ -31,11 +31,23 @@ in
     shell = "${pkgs.zsh}/bin/zsh";
     terminal = "tmux-256color";
     historyLimit = 65535;
-    
+
     plugins = with pkgs.tmuxPlugins; [
       cpu
-      resurrect
-      continuum
+      {
+        plugin = resurrect;
+        extraConfig = "
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+        ";
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '5' # minutes
+        '';
+      }
       tmux-nerd-font-window-name
       tmux-nvim
     ];
