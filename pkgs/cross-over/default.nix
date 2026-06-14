bad8af29f24b7d6228eb8155622e0364bed32862
msdone <lwt6077@gmail.com>
2026-06-14 22:59:57 +0800

    feat(crossover): add executable wrapper with Wayland support

--- pkgs/cross-over/default.nix
+++ pkgs/cross-over/default.nix
@@ -95,6 +95,15 @@ stdenv.mkDerivation (finalAttrs: {
     runHook postInstall
   '';
 
+  postFixup = ''
+    makeWrapper ${lib.getExe electron} "$out/bin/crossover" \
+      --add-flags "$out/share/crossover/resources/app.asar" \
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
+      --set-default ELECTRON_FORCE_IS_PACKAGED 1 \
+      --set-default ELECTRON_IS_DEV 0 \
+      --inherit-argv0
+  '';
+
   desktopItems = [
     (makeDesktopItem {
       name = "crossover";
